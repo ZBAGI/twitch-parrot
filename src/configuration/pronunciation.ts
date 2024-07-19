69cec93d55d74af395222d4701763a333b09c5d9
ZBAGI <zbagi@poczta.fm>
2024-07-20 00:09:50 +0200

    added fix for pronouncing name with @

--- src/configuration/pronunciation.ts
+++ src/configuration/pronunciation.ts
@@ -4,10 +4,10 @@ export class Pronunciation extends FileMap<string, string> {
 	public apply(text: string): string {
 		const words = text.split(" ");
 		const replacedWords = words.map(word => {
-			const lowerCaseWord = word.toLowerCase();
-			if (this.has(lowerCaseWord))
-				return this.get(lowerCaseWord) || word;
-			return word;
+			let lowerCaseWord = word.toLowerCase();
+			// Common case of the pronunciation is user nicknames, but if we pronounce ABC as CC we need also pronounce @ABC as @CCC
+			lowerCaseWord = lowerCaseWord.startsWith("@") ? lowerCaseWord.slice(1) : lowerCaseWord;
+			return this.get(lowerCaseWord) || word;
 		});
 		
 		return replacedWords.join(' ');
